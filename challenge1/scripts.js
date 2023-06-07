534954c63b4ffe653c41e3a825af1a3a9fab75e2
Mischka van Wyk <m.vanwyk@lab.clicklearning.org>
2023-06-07 21:26:15 +0200

    initial commit IWA14 challenge 1&2

---
+++ challenge1/scripts.js
@@ -0,0 +1,14 @@
+const firstName = 'John';
+const age = 35;
+hobby = 'Coding';
+
+const logTwice = () => {
+  console(parameter)
+  console(parameter)
+}
+
+function hobby (hobby) {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+hobby()
